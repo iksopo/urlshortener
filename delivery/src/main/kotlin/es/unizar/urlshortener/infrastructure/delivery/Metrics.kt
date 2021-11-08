05530c6924792e603a4f8d1d0cebd166767feb88
CursedR3N <r3nstark@gmail.com>
2021-11-08 20:58:29 +0100

    Memory metrics

--- delivery/src/main/kotlin/es/unizar/urlshortener/infrastructure/delivery/Metrics.kt
+++ delivery/src/main/kotlin/es/unizar/urlshortener/infrastructure/delivery/Metrics.kt
@@ -3,14 +3,17 @@ package es.unizar.urlshortener.infrastructure.delivery
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
 
+import java.lang.Runtime
+
 @Controller
 class Metrics {
 	@GetMapping("/metrics")
 	fun metrics(model: MutableMap<String, Any>): String {
-		model["uptime"] = 1
-		model["redTime"] = 2
-		model["memUsed"] = 3
-		model["memFree"] = 4
+		var rt = Runtime.getRuntime()
+		model["uptime"] = "TODO"
+		model["redTime"] = "TODO"
+		model["memUsed"] = ((rt.totalMemory()-rt.freeMemory())/1000000).toString() + "MB"
+		model["memFree"] = (rt.freeMemory()/1000000).toString() + "MB"
 		return "metrics"
 	}
 }
