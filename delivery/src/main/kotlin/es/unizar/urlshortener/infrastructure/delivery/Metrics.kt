1054d085f1078f16531b62bec104d7e01d65d528
CursedR3N <r3nstark@gmail.com>
2021-11-09 17:12:59 +0100

    uptime

--- delivery/src/main/kotlin/es/unizar/urlshortener/infrastructure/delivery/Metrics.kt
+++ delivery/src/main/kotlin/es/unizar/urlshortener/infrastructure/delivery/Metrics.kt
@@ -4,13 +4,18 @@ import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.*
 
 import java.lang.Runtime
+import kotlin.time.toDuration
 
 @Controller
 class Metrics {
+
+	var startTime = System.currentTimeMillis()
+
 	@GetMapping("/metrics")
 	fun metrics(model: MutableMap<String, Any>): String {
 		var rt = Runtime.getRuntime()
-		model["uptime"] = "TODO"
+		var uts = (System.currentTimeMillis()-startTime)/1000.0
+		model["uptime"] = ((uts/3600)%24).toInt().toString() + ":" + ((uts/60)%60).toInt().toString() + ":" + (uts%60).toInt().toString()
 		model["redTime"] = "TODO"
 		model["memUsed"] = ((rt.totalMemory()-rt.freeMemory())/1000000).toString() + "MB"
 		model["memFree"] = (rt.freeMemory()/1000000).toString() + "MB"
