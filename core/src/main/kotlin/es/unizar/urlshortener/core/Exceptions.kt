e25aa59992c11e5d81f91b38777559183c237835
María Peña <780448@unizar.es>
2022-01-02 17:38:15 +0100

    Left uses and expiration date added to CSVs

--- core/src/main/kotlin/es/unizar/urlshortener/core/Exceptions.kt
+++ core/src/main/kotlin/es/unizar/urlshortener/core/Exceptions.kt
@@ -6,5 +6,7 @@ class RedirectionNotFound(val key: String) : Exception("[$key] is not known")
 
 class InvalidTypeOfFile(val filename: String?) : Exception("The format of [$filename] is not supported, only .csv files are")
 
+class WrongStructuredFile(val filename: String) : Exception("The structure of [$filename] is invalid, each line must consist of three comma-separated fields, even if they are empty")
+
 class FileDoesNotExist(val filename: String) : Exception(
     "The file [$filename] doesn't exist. If you couldn't download the new CSV, try regenerating it")
--- core/src/main/kotlin/es/unizar/urlshortener/core/usecases/CreateShortUrlsFromCsvUseCase.kt
+++ core/src/main/kotlin/es/unizar/urlshortener/core/usecases/CreateShortUrlsFromCsvUseCase.kt
@@ -6,7 +6,11 @@ import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 import org.springframework.web.multipart.MultipartFile
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+import java.util.concurrent.atomic.AtomicBoolean
 import java.util.concurrent.atomic.AtomicInteger
+import kotlin.collections.ArrayList
 
 /**
  * Given a file creates a short URL for each url in the file
@@ -36,24 +40,69 @@ class CreateShortUrlsFromCsvUseCaseImpl(
         val mutex = Mutex()
         val newName = "${fileStorage.generateName()}.csv"
         fileStorage.store(file, newName)
-        val lines = fileStorage.readLines(newName)
+        val lines = fileStorage.readLines(newName).filter { l -> l != "" }
         val numUrls = lines.size
+        val wellFormatted = AtomicBoolean(true)
+        val allWrongFormatted = AtomicBoolean(numUrls != 0)
+            // If there are no urls to shorten, the format is always valid
+            // Otherwise it is invalid until a valid line is found
         runBlocking {
             for (line in lines) {
                 launch {
-                    try {
-                        createShortUrlUseCase.create(
-                            url = line,
-                            data = ShortUrlProperties(
-                                ip = remoteAddr
+                    val lineSplitByComma = line.split(",")
+                    if (lineSplitByComma.size != 3) {
+                        wellFormatted.set(false)
+                        mutex.withLock {
+                            shortUrls.add(
+                                Pair(
+                                    ShortUrl(
+                                        hash = "", redirection = Redirection(lineSplitByComma[0]),
+                                        properties = ShortUrlProperties(safe = false, ip = remoteAddr)
+                                    ), "Invalid structure of line, each line must consist of three comma-separated fields, even if they are empty"
+                                )
                             )
-                        ).let {
-                            mutex.withLock { shortUrls.add(Pair(it, null)) }
                         }
-                    } catch (ex: InvalidUrlException) {
-                        mutex.withLock { shortUrls.add(Pair(ShortUrl(hash = "", redirection = Redirection(line),
-                                properties = ShortUrlProperties(safe = false, ip = remoteAddr)), ex.message)) }
-
+                    } else {
+                        allWrongFormatted.set(false)
+                        val url = lineSplitByComma[0]
+                        val leftUses = lineSplitByComma[1].toIntOrNull() // TODO: throw if it's not ""
+                        try {
+                            val expiration = lineSplitByComma[2].let { date ->
+                                if (date == "") {
+                                    null
+                                } else {
+                                    java.sql.Date.valueOf(
+                                        LocalDate.parse(
+                                            lineSplitByComma[2],
+                                            DateTimeFormatter.ISO_OFFSET_DATE_TIME
+                                        )
+                                    )
+                                }
+                            }
+                            println(expiration)
+                            //LocalDate.parse(lineSplitByComma[2], DateTimeFormatter.ISO_OFFSET_DATE_TIME) //OffsetDateTime.parse(lineSplitByComma[2])
+                            createShortUrlUseCase.create(
+                                url = url,
+                                data = ShortUrlProperties(
+                                    ip = remoteAddr,
+                                    leftUses = leftUses,
+                                    expiration = expiration
+                                )
+                            ).let {
+                                mutex.withLock { shortUrls.add(Pair(it, null)) }
+                            }
+                        } catch (ex: InvalidUrlException) {
+                            mutex.withLock {
+                                shortUrls.add(
+                                    Pair(
+                                        ShortUrl(
+                                            hash = "", redirection = Redirection(url),
+                                            properties = ShortUrlProperties(safe = false, ip = remoteAddr)
+                                        ), ex.message
+                                    )
+                                )
+                            }
+                        }
                     }
                     val progress = counter.incrementAndGet() * 100 / numUrls
                     listener.onProgress(progress)
@@ -61,6 +110,11 @@ class CreateShortUrlsFromCsvUseCaseImpl(
             }
         }
         listener.onCompletion()
+        if (!wellFormatted.get() && allWrongFormatted.get()) {
+            println(nameSplitByPoint.joinToString("."))
+            fileStorage.deleteFile(newName)
+            throw WrongStructuredFile(nameSplitByPoint.joinToString("."))
+        }
         return FileAndShortUrl(filename = newName, urls = shortUrls)
     }
 }
